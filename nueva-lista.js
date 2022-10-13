e4c370440ff374ec5630e34e26be864e32a7b0bf
miguel <miguelcervantes.re@gmail.com>
2022-10-13 15:47:31 -0500

    mi primera lista

---
+++ nueva-lista.js
@@ -0,0 +1,16 @@
+
+const datos= [ 
+
+ " nombre = miguel ",
+ " edad = 29 ",
+ " fecha_de_nacimiento = 19/05/1993",
+
+   libro={
+    titulo:"Harry Potter y la piedra filosofal",
+    autor: "J. K. Rowling",
+    fecha:"26 de junio de 1997",
+}
+
+]
+
+console.log(datos);
